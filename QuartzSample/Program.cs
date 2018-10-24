eb7c99d22669d9a3bbd99e48b4bed8b60aeee060
Faisal Nadeem <Faisal.Nadeem@callcreditgroup.com>
2018-10-24 15:34:35 +0100

    quartz scheduler

---
+++ QuartzSample/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace QuartzSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var schedulerFactory = new StdSchedulerFactory();
+
+            var scheduler = schedulerFactory.GetScheduler().Result;
+            scheduler.Start();
+            var jobDetail = JobBuilder.Create<ConsoleWriterJob>().WithIdentity("job-wr", "writeconsole").Build();
+            var trigger =
+                TriggerBuilder.Create().WithIdentity("job-wr-trigger", "writeconsole")
+                    .WithSimpleSchedule(x => x.WithIntervalInSeconds(2).RepeatForever())
+                    .Build();
+            scheduler.ScheduleJob(jobDetail, trigger);
+
+            var addJobDetail = JobBuilder.Create<AddNumbersJob>().WithIdentity("add-numbers-job", "arithmatics").Build();
+            var addJobTrigger =
+                TriggerBuilder.Create().WithIdentity("add-numbers-job-trigger", "arithmatics")
+                    .WithSimpleSchedule(x => x.WithIntervalInSeconds(3).RepeatForever())
+                    .Build();
+            scheduler.ScheduleJob(addJobDetail, addJobTrigger);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            scheduler.Shutdown();
+        }
+    }
+
+    public class ConsoleWriterJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            return WriteLineTask();
+        }
+
+        private static async Task WriteLineTask()
+        {
+            Console.WriteLine($"Time now {DateTime.Now}");
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            return;
+        }
+    }
+    public class AddNumbersJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            return AddTask(2, 3);
+        }
+
+        private static async Task<int> AddTask(int x, int y)
+        {
+            Console.WriteLine($"Adding numbers at {DateTime.Now}: {x} + {y} : {x + y}");
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            return x + y;
+        }
+    }
+
+}
