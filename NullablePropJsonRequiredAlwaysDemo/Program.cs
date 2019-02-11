b2fa722869c2fde05cd5ab67ffa2537e894c79da
Faisal Nadeem <faisal.nadeem@callcreditgroup.com>
2019-02-11 17:47:13 +0000

    nullable property with json property attribute (Required.Always)

---
+++ NullablePropJsonRequiredAlwaysDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace NullablePropJsonRequiredAlwaysDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			string json = @"{
+							'Name': 'Starcraft III',
+							'gameId': 0,
+							'ReleaseDate': null
+							}";
+
+			var starcraft = JsonConvert.DeserializeObject<Videogame>(json);
+
+			Console.WriteLine(starcraft.ToString());
+
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+	}
+
+	public class Videogame
+	{
+		[JsonProperty(Required = Required.Always)]
+		public string Name { get; set; }
+
+		[JsonProperty(Required = Required.Always)]
+		public int? GameId { get; set; }
+
+		[JsonProperty(Required = Required.AllowNull)]
+		public DateTime? ReleaseDate { get; set; }
+
+		public override string ToString()
+		{
+			return $"Game Id:{GameId}, Name:{Name}, ReleaseDate:{ReleaseDate}";
+		}
+	}
+}
