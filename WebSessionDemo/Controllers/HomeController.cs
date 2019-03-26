bba0e7e217ae4fc2aef8a2ed865ff5298669f920
Faisal Nadeem <faisal.nadeem@callcreditgroup.com>
2019-03-26 12:46:44 +0000

    crossdomain cookies

--- WebSessionDemo/Controllers/HomeController.cs
+++ WebSessionDemo/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Security.Policy;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using WebSessionDemo.Models;
 
 namespace WebSessionDemo.Controllers
 {
@@ -23,6 +24,39 @@ namespace WebSessionDemo.Controllers
 
 			var isok = currentHostname.Contains(configHostName, StringComparison.OrdinalIgnoreCase);
 			return View();
+		}
+
+		public ActionResult TestCrossDomainCookie()
+		{
+			var model = new CrossDomainCookieViewModel() {Name = "XDomainCookieVm", Description = "XDomainCookieVmDesc"};
+
+			return View(model);
+		}
+		
+		[HttpPost]
+		public ActionResult TestCrossDomainCookie(CrossDomainCookieViewModel model)
+		{
+			string cookieName = "cookiename";
+			string uid = "uidddd";
+			string pwd = "peessleeiisl";
+			string cookiePath = "pathatest";
+			string cookieDomain = ".noddle.co.uk";
+			string cookieSecure = "true";
+
+			Response.AddHeader("Set-Cookie",
+				$"{cookieName}={uid}|{pwd};path={cookiePath}domain={cookieDomain}secure={cookieSecure}");
+
+			//var httpCookie = new HttpCookie("xdomain-cookie", model.Description)
+			//{
+			//	Domain = ".noddle.co.uk",
+			//	HttpOnly = true,
+			//	Expires = DateTime.Now.AddHours(1)
+			//};
+
+			//Response.Cookies.Add(httpCookie);
+			//HttpContext.Current.Response.Cookies.Add(httpCookie);
+			return RedirectPermanent("https://www.noddle.co.uk/account/sign-in");
+			//return View(model);
 		}
 
 		public async Task<ActionResult> TestIndexAsync()
---
+++ WebSessionDemo/Models/CrossDomainCookieViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSessionDemo.Models
+{
+	public class CrossDomainCookieViewModel
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
