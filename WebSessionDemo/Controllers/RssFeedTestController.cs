6830308acd19d8124db6f4bdb5191bfed1dfa78e
Faisal Nadeem <faisal.nadeem@callcreditgroup.com>
2019-04-03 14:08:46 +0100

    custom dotnet core template, syndication category test controller

---
+++ WebSessionDemo/Controllers/RssFeedTestController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace WebSessionDemo.Controllers
+{
+    public class RssFeedTestController : Controller
+    {
+        // GET: RSSFeedTest
+        public ActionResult Index()
+        {
+		    var parliamentNewUrsl = "https://www.parliament.uk/g/RSS/news-feed/?pageInstanceId=209&limit=20";
+	        var newsFeed = GetAsync(parliamentNewUrsl);
+
+			//	        var feed = new RssFeedTestViewModel
+			//	        {
+			//				Channel = new List<RssChannelItem>
+			//				{
+			//					new RssChannelItem{Title = }
+			//				}
+			//,
+			//	        };
+			return View();
+        }
+
+	    static async Task<RssFeedTestViewModel> GetAsync(string path)
+	    {
+		    RssFeedTestViewModel feedAsync = null;
+		    var httpClient = new HttpClient();
+		    httpClient.DefaultRequestHeaders.Accept.Clear();
+			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+		    HttpResponseMessage response = await httpClient.GetAsync(path);
+		    if (response.IsSuccessStatusCode)
+		    {
+			    feedAsync = await response.Content.ReadAsAsync<RssFeedTestViewModel>();
+		    }
+		    return feedAsync;
+	    }
+	}
+
+	public class RssFeedTestViewModel
+	{
+		public IEnumerable<RssChannelItem> Item { get; set; }
+	}
+
+	public class RssChannelItem
+	{
+		public string Title { get; set; }
+	}
+}
---
+++ WebSessionDemo/Controllers/SyndicationTestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSessionDemo.Controllers
+{
+    public class SyndicationTestController : Controller
+    {
+		// GET: SyndicationTest
+		public ActionResult Index()
+        {
+			//https://docs.microsoft.com/en-us/dotnet/api/system.servicemodel.syndication.syndicationcategory?view=netframework-4.7.2
+	        SyndicationFeed myFeed = new SyndicationFeed("My Test Feed",
+		        "This is a test feed",
+		        new Uri("http://FeedServer/Test"), "MyFeedId", DateTime.Now);
+	        SyndicationItem myItem = new SyndicationItem("Item One Title",
+		        "Item One Content",
+		        new Uri("http://FeedServer/Test/ItemOne"));
+	        myItem.Categories.Add(new SyndicationCategory("MyCategory"));
+	        Collection<SyndicationItem> items = new Collection<SyndicationItem>();
+	        items.Add(myItem);
+	        myFeed.Items = items;
+			return View(myFeed);
+        }
+    }
+}
